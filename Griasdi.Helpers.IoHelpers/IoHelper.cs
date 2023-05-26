4ffbc878c5e3bf1d82971f679e4bfe548fd1bb2e
makersdelite <makersdelite@gmail.com>
2023-05-26 07:54:12 +0200

    Griasdi.Helpers.IoHelpers introduced locally stored as subfolder of git controlled solution folder for - hopefully - included in github repo automatically

---
+++ Griasdi.Helpers.IoHelpers/IoHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Griasdi.Helpers.IoHelpers
+{
+    public static class IoHelper
+    {
+        public static async Task<bool> SaveAsync()
+        {
+            bool retVal = false;
+            await Task.Delay(0);
+            return retVal;
+        }
+    }
+}
